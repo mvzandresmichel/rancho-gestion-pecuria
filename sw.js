15143c55ebab36659443004d30997e4889154695
mvzandresmichel <mvzandresmichel@gmail.com>
2026-09-04 23:27:20 -0600

    Actualizar a Rancho Gestion Pecuaria V6.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rancho-v62-cache';
+const CACHE='rancho-v63-cache';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -7,13 +7,8 @@ self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
   if(url.origin!==self.location.origin) return;
   if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{
-      const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res;
-    }).catch(()=>caches.match('./index.html')));
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res;}).catch(()=>caches.match('./index.html')));
     return;
   }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    if(res.ok){const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy));}
-    return res;
-  }).catch(()=>caches.match('./index.html'))));
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>caches.match('./index.html'))));
 });
